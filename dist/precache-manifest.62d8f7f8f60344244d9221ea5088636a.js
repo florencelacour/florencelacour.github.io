8ce07ca7a1d988b58338c592150ad35e2ead0312
Florence <lacour.florence@gmail.com>
2019-09-13 18:07:47 +0200

    :rocket: Add dist files

---
+++ dist/precache-manifest.62d8f7f8f60344244d9221ea5088636a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7375a36d3a45d8ac8c7e",
+    "url": "/js/chunk-vendors.d4fb9efb.js"
+  },
+  {
+    "revision": "f094e17fdfd48163ab95",
+    "url": "/js/app.e34ce1d0.js"
+  },
+  {
+    "revision": "a5350a2a192c639f8213",
+    "url": "/js/about.a1a680d9.js"
+  },
+  {
+    "revision": "1227ea4de960b2c0bd3056d674e1d27d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f094e17fdfd48163ab95",
+    "url": "/css/app.d3f49cf2.css"
+  }
+];
